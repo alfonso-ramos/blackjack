dcb9710ab8c50722e9e71043ecb4a37a66111096
Alfonso <alfonsoramos.1803@gmail.com>
2023-10-09 10:09:32 -0700

    [ADD] agregada funcionalidad para detener el juego y que la computadora compita con el jugador mostrando sus cartas y puntaje, tambien se agregaron las alertas que indican quien gano o si el juego termino en un empate

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,21 +7,25 @@ let puntosComputadora = 0
 
 // Referencias en el DOM
 const btnPedir = document.querySelector('#btnPedir')
+const btnDetener = document.querySelector('#btnDetener')
 
 const cartasJugador = document.querySelector('#jugador-cartas')
+const cartasComputadora = document.querySelector("#computadora-cartas")
+
+// const advertisement = document.querySelector("#advertisement")
 
 let puntajes = document.querySelectorAll('small')
 
 const crearDeck = () => {
-    for( let i = 2; i <= 10; i++) {
-        for( let tipo of tipos){
-            deck.push( i + tipo)
+    for (let i = 2; i <= 10; i++) {
+        for (let tipo of tipos) {
+            deck.push(i + tipo)
         }
     }
 
-    for (let tipo of tipos){
-        for(let esp of especiales){
-            deck.push( esp + tipo)
+    for (let tipo of tipos) {
+        for (let esp of especiales) {
+            deck.push(esp + tipo)
         }
     }
 
@@ -32,7 +36,7 @@ crearDeck()
 
 // Funcion para pedir carta
 const pedirCarta = () => {
-    if ( deck.length === 0 ){
+    if (deck.length === 0) {
         throw 'No hay mas cartas en el deck'
     }
     const ultimaCarta = deck.pop()
@@ -44,9 +48,9 @@ const pedirCarta = () => {
 const valorCarta = (carta) => {
     const valor = carta.substring(0, carta.length - 1)
 
-    return ( isNaN(valor) ) ?
-            ( valor === 'A' ) ? 11 : 10
-            : valor * 1
+    return (isNaN(valor)) ?
+        (valor === 'A') ? 11 : 10
+        : valor * 1
 
     // if(isNaN(valor)) {
     //     puntos = (valor === 'A') ? 11 : 10
@@ -55,8 +59,44 @@ const valorCarta = (carta) => {
     // }
     // return valor
 }
+
+// Turno de la computadora
+const turnoComputadora = (puntosMinimos) => {
+    do {
+        const carta = pedirCarta()
+        puntosComputadora = puntosComputadora + valorCarta(carta)
+        puntajes[1].innerText = puntosComputadora
+
+        const imgCarta = document.createElement('img')
+        imgCarta.src = `../cartas/${carta}.png`
+        imgCarta.classList.add('carta')
+        cartasComputadora.append(imgCarta)
+
+        if (puntosJugador > 21) {
+            break
+        }
+    } while (
+        (puntosComputadora < puntosMinimos) && (puntosMinimos <= 21)
+    )
+
+    // Alertas que anuncian quien gano el juego
+
+    setTimeout(() => {
+        if (puntosComputadora === puntosMinimos) {
+            alert('Empate')
+        } else if (puntosMinimos > 21) {
+            alert('Computadora Gana')
+        } else if (puntosComputadora > 21) {
+            alert('Jugador Gana')
+        } else {
+            alert('Empate')
+        }
+    }, 10)
+}
+
 // Eventos
 
+// Evento para que el usuario tome una carta
 btnPedir.addEventListener('click', () => {
     const carta = pedirCarta()
     puntosJugador = puntosJugador + valorCarta(carta)
@@ -67,10 +107,18 @@ btnPedir.addEventListener('click', () => {
     imgCarta.classList.add('carta')
     cartasJugador.append(imgCarta)
 
-    if( puntosJugador > 21) {
+    if (puntosJugador > 21) {
         btnPedir.disabled = true
-    } else if (puntosJugador === 21){
+        btnDetener.disabled = true
+        turnoComputadora(puntosJugador)
+    } else if (puntosJugador === 21) {
         btnPedir.disabled = true
+        btnDetener.disabled = true
     }
 })
 
+btnDetener.addEventListener('click', () => {
+    btnPedir.disabled = true
+    btnDetener.disabled = true
+    turnoComputadora(puntosJugador)
+})
